cf77701dba59f6f5ef57e62243b57576a1f23c17
PlumaBrava <perez.juan.jose@gmail.com>
2016-01-21 19:44:06 -0300

    ADD Cloud to Aplication

--- app/src/main/java/com/nextnut/final2/MainActivity.java
+++ app/src/main/java/com/nextnut/final2/MainActivity.java
@@ -1,9 +1,11 @@
 package com.nextnut.final2;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -59,6 +61,8 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         Log.i("Final1", "iniADS");
         mAdView.loadAd(adRequest);
+
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
 
     }
 
---
+++ backend/src/main/java/com/example/perez/juan/jose/backend/MyBean.java
@@ -0,0 +1,15 @@
+package com.example.perez.juan.jose.backend;
+
+/** The object model for the data we are sending through endpoints */
+public class MyBean {
+
+    private String myData;
+
+    public String getData() {
+        return myData;
+    }
+
+    public void setData(String data) {
+        myData = data;
+    }
+}
