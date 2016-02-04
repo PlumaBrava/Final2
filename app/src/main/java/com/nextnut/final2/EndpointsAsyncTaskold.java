ea155a1093c68c38dab080cfe50913e4289d8d04
PlumaBrava <perez.juan.jose@gmail.com>
2016-02-04 14:58:59 -0300

    Saca de cloud el nombre, retorna solo Joke

--- app/src/main/java/com/nextnut/final2/EndpointsAsyncTaskold.java
+++ app/src/main/java/com/nextnut/final2/EndpointsAsyncTaskold.java
@@ -15,6 +15,8 @@ import com.example.perez.juan.jose.backend.myApi.MyApi;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.nextnut.mylibrary.MainActivityLibrary;
 
 
@@ -55,22 +57,22 @@ public class EndpointsAsyncTaskold extends AsyncTask<Pair<Context, String>,Void,
 
         Log.i("AsyncTaskOld", "doInBackground");
         if (myApiService == null) {  // Only do this once
-//            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-//                    new AndroidJsonFactory(), null)
-//                    // options for running against local devappserver
-//                    // - 10.0.2.2 is localhost's IP address in Android emulator
-//                    // - turn off compression when running against local devappserver
-//                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-//                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-//                        @Override
-//                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-//                            abstractGoogleClientRequest.setDisableGZipContent(true);
-//                        }
-//                    });
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    // options for running against local devappserver
+                    // - 10.0.2.2 is localhost's IP address in Android emulator
+                    // - turn off compression when running against local devappserver
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
 
-
-            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
-                    .setRootUrl("https://final2-1197.appspot.com/_ah/api/");
+//
+//            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+//                    .setRootUrl("https://final2-1197.appspot.com/_ah/api/");
 
 
             // end options for devappserver
@@ -85,7 +87,7 @@ public class EndpointsAsyncTaskold extends AsyncTask<Pair<Context, String>,Void,
 
             try {
                 Log.i("AsyncTaskOld", "doInBackground-call service");
-                return myApiService.sayHi("j").execute().getData();
+                return myApiService.sayHi().execute().getData();
             } catch (IOException e) {
                 Log.i("AsyncTaskOld", "doInBackground-exception");
                 return e.getMessage();
--- backend/src/main/java/com/example/perez/juan/jose/backend/MyEndpoint.java
+++ backend/src/main/java/com/example/perez/juan/jose/backend/MyEndpoint.java
@@ -27,11 +27,13 @@ public class MyEndpoint {
 
     /** A simple endpoint method that takes a name and says Hi back */
     @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
+    public MyBean sayHi() {
         MyBean response = new MyBean();
-        response.setData("Hi, " + name+new Joke().getJoke());
+        response.setData(new Joke().getJoke());
 
         return response;
     }
+
+
 
 }
